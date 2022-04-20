dbf2af8bdf49422fa9565fa06630403ece59b3e2
Andrea Gonzalez <ggarciaandrea@gmail.com>
2022-04-20 02:19:47 +0200

    empezando el diseño del proyecto

---
+++ html/header.php
@@ -0,0 +1,9 @@
+<?php echo '
+<div>
+  <picture>
+    <source media="(max-width: 780px)" srcset="./resources/logo-small.png" " />
+    <img src="./resources/logo-big.png" alt="Logo de Lostark" />
+  </picture>
+</div>
+<div><button type="button">Perfil</button></div>
+'?>
